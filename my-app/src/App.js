23acc23c9294660a3af708ac59901c6e412d6918
Kiran <kirankumardn74@gmail.com>
2024-06-06 14:52:39 +0530

    Files added

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
